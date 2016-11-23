ec8e94bb5ae355e1d5cbdee3fdfcbbf2dacd0d51
Steven Yang <yang.steven08@gmail.com>
2016-11-22 20:09:19 -0600

    Completed Feed UI

--- SocialMedia/CircleImageView.swift
+++ SocialMedia/CircleImageView.swift
@@ -1,14 +1,14 @@
 //
-//  FancyView.swift
+//  CircleImageView.swift
 //  SocialMedia
 //
-//  Created by Steven Yang on 11/20/16.
+//  Created by Steven Yang on 11/22/16.
 //  Copyright © 2016 Steven Yang. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleImageView: UIImageView {
 
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -17,7 +17,12 @@ class FancyView: UIView {
     layer.shadowOpacity = 0.8
     layer.shadowRadius = 5.0
     layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-    layer.cornerRadius = 2.0
+  }
+  
+  override func draw(_ rect: CGRect) {
+    super.draw(rect)
+    
+    layer.cornerRadius = self.frame.width / 2
   }
 
 }
--- SocialMedia/FeedViewController.swift
+++ SocialMedia/FeedViewController.swift
@@ -25,14 +25,6 @@ class FeedViewController: UIViewController {
     try! FIRAuth.auth()?.signOut()
     dismiss(animated: true, completion: nil)
   }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
