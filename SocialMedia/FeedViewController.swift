046775e50f15e07b516cbb6ba6679a9c00570451
Steven Yang <yang.steven08@gmail.com>
2016-12-29 14:56:39 -0600

    Completed image upload to storage

--- SocialMedia/FeedViewController.swift
+++ SocialMedia/FeedViewController.swift
@@ -14,10 +14,13 @@ class FeedViewController: UIViewController {
 
   @IBOutlet weak var tableView: UITableView!
   @IBOutlet weak var imageAddButton: CircleImageView!
+  @IBOutlet weak var captionField: FancyTextField!
+  
   
   var posts = [Post]()
   var imagePicker: UIImagePickerController!
   static var imageCache: NSCache<NSString, UIImage> = NSCache()
+  var imageSelected = false
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -58,16 +61,48 @@ class FeedViewController: UIViewController {
     present(imagePicker, animated: true, completion: nil)
   }
   
+  @IBAction func postButtonTapped(_ sender: Any) {
+
+    let ok = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+    
+    guard let caption = captionField.text, caption != "" else {
+      print("STEVEN: Caption must be entered")
+      let alert = UIAlertController(title: "Caption must be entered", message: nil, preferredStyle: .alert)
+      alert.addAction(ok)
+      present(alert, animated: true, completion: nil)
+      return
+    }
+    
+    guard let img = imageAddButton.image, imageSelected == true else {
+      print("STEVEN: Image must be selected")
+      let alert = UIAlertController(title: "Image must be selected", message: nil, preferredStyle: .alert)
+      alert.addAction(ok)
+      present(alert, animated: true, completion: nil)
+      return
+    }
+    
+    if let imgData = UIImageJPEGRepresentation(img, 0.2) {
+      let imageUID = NSUUID().uuidString
+      let metaData = FIRStorageMetadata()
+      // Turn image into jpeg
+      metaData.contentType = "image/jpeg"
+      
+      DataService.ds.REF_POST_IMAGES.child(imageUID).put(imgData, metadata: metaData) { (metadata, error) in
+        if error != nil {
+          print("STEVEN: Unable to upload image to Firebase storage")
+        } else {
+          print("STEVEN: Successfully uploaded image to Firebase storage")
+          let downloadURL = metaData.downloadURL()?.absoluteString
+        }
+      }
+    }
+    
+  }
 
 }
 
-extension FeedViewController: UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-  
-  func numberOfSections(in tableView: UITableView) -> Int {
-    
-    return 1
-  }
-  
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
     return posts.count
@@ -93,20 +128,21 @@ extension FeedViewController: UITableViewDelegate, UITableViewDataSource, UIImag
       
       return PostCellTableViewCell()
     }
-    
   }
+  
+}
+
+extension FeedViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
     if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
       imageAddButton.image = image
+      imageSelected = true
     } else {
       print("STEVEN: A valid image wasn't selected")
     }
     
     imagePicker.dismiss(animated: true, completion: nil)
   }
-  
-  
-  
-  
+
 }
